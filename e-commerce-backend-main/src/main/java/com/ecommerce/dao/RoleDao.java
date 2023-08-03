ffdc1471ea77e91140ef000bac844fdbb5fe55a0
Vivekanand Singh <94869672+vivekanandsh330@users.noreply.github.com>
2023-08-03 18:48:05 +0530

    Add files via upload

---
+++ e-commerce-backend-main/src/main/java/com/ecommerce/dao/RoleDao.java
@@ -0,0 +1,10 @@
+package com.ecommerce.dao;
+
+import com.ecommerce.entity.Role;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleDao extends CrudRepository<Role, String> {
+
+}
